756c9618abe04983f61ea66ddcecba4e82931c49
Amit Sarker <amitcse1995@gmail.com>
2022-04-23 10:57:28 +0600

    Init empty project.
    Created an empty project with minimum deployment target 14.0

---
+++ ExerciseViewer/ExerciseViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExerciseViewer
+//
+//  Created by Amit Sarker on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
